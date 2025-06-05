9d54f46bf3a533a8fd1b904f39fe3898f8a686d8
Prabh <prabh@gmail.com>
2025-06-05 10:58:50 +0530

    db js and insertdata file added

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,12 @@
-// connecting to the database - mongo db
+import pg from "pg";
+
+const db = new pg.Client({
+  user : "postgres",
+  host : "localhost",
+  database : "codingweb",
+  password : "prabh@singh",
+  port : 5000,
+});
+db.connect();
+
+export default db;
---
+++ backend/insertdata.js
@@ -0,0 +1,13 @@
+import db from "./db.js";
+async function adduser(userid , username , user_email , password){
+    try{
+        const query = `insert into users(userid , username , user_email , password) 
+        values($1 , $2 , $3 , $4) , [userid , username , user_email , password];
+        `;
+        const values = [userid , username , user_email , password];
+        const res = await db.query(query , values);
+        console.log("user data inserted : " , res.rows[0]);
+    } catch(err) {
+      console.error("error while iserting user ..... INVALID DATA : " , err);
+    }
+}
